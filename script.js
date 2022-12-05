606b4bf729cf9e1f197e8b8092110394187c257a
Danny Infurna <f.danny95@gmail.com>
2022-12-05 10:58:05 +0100

    Inizio ad usare Git

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    let door = document.querySelector('.door');
+    let doorChild = door.children
+        door.addEventListener('click', ()=>{
+
+            if(!document.querySelector('.left')){
+                
+                for(let i=0; i<doorChild.length; i++){
+                    if(doorChild[i].src.includes('left')){
+                        doorChild[i].setAttribute('class', 'left');
+                    }else{
+                        doorChild[i].setAttribute('class', 'right');
+                    }
+                }
+            } else{
+                for(let i=0; i<doorChild.length; i++){
+                    if(doorChild[i].src.includes('left')){
+                        doorChild[i].removeAttribute('class', 'left');
+                    }else{
+                        doorChild[i].removeAttribute('class', 'right');
+                    }
+                }
+            }
+        })
+})
