07d2b0e39f2dd88390f768fc8ada27b6f57f063b
Danny Infurna <f.danny95@gmail.com>
2023-08-25 12:01:18 +0200

    fix: improved doors

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    let door = document.querySelector('.door');
+document.addEventListener("DOMContentLoaded", () => {
+  const door = document.querySelector(".doors");
+  const left_doors = document.querySelectorAll(".door-left");
+  const right_doors = document.querySelectorAll(".door-right");
 
-    /* Al click apro le porte di "my Skills" */
-    let doorChild = door.children
-        door.addEventListener('click', ()=>{
+  let multiplyerDoorLeft = 1;
+  let multiplyerDoorRight = 1;
 
-            if(!document.querySelector('.left')){
-                for(let i=0; i<doorChild.length; i++){
-                    if(doorChild[i].src.includes('left')){
-                        doorChild[i].setAttribute('class', 'left');
-                    }else{
-                        doorChild[i].setAttribute('class', 'right');
-                    }
-                }
-            } else{
-                for(let i=0; i<doorChild.length; i++){
-                    if(doorChild[i].src.includes('left')){
-                        doorChild[i].removeAttribute('class', 'left');
-                    }else{
-                        doorChild[i].removeAttribute('class', 'right');
-                    }
-                }
-            }
-        })
-})
+  /* Al click apro le porte di "my Skills" */
+  door.addEventListener("click", () => {
+    console.log(document.querySelector(".left"));
+    if (left_doors[0].style.transform !== "") {
+      closeDoors();
+    } else openDoors();
+
+    multiplyerDoorLeft = 0;
+    multiplyerDoorRight = 0;
+  });
+
+  function openDoors() {
+    for (let i = left_doors.length - 1; i >= 0; i--) {
+      setTimeout(() => {
+        left_doors[i].style.transform = "translate(-550px)";
+      }, multiplyerDoorLeft * 200);
+
+      multiplyerDoorLeft++;
+    }
+    for (let i = 0; i < right_doors.length; i++) {
+      setTimeout(() => {
+        right_doors[i].style.transform = "translate(550px)";
+      }, multiplyerDoorRight * 200);
+
+      multiplyerDoorRight++;
+    }
+  }
+
+  function closeDoors() {
+    for (let i = left_doors.length - 1; i >= 0; i--) {
+      setTimeout(() => {
+        left_doors[i].style.transform = "translate(0px)";
+        right_doors[i].style.transform = "translate(0px)";
+      }, multiplyerDoorLeft * 200);
+    }
+    multiplyerDoorLeft = 0;
+    multiplyerDoorRight = 0;
+  }
+});
